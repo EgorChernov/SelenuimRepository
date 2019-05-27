fd7eec6439b94c57fe725da1687093c4d74fa357
e.chernov <e.chernov@01DEV78>
2019-05-27 03:17:45 -0700

    add test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new ChromeDriver();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Url = "https://www.google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("google");
+            driver.FindElement(By.Name("btnK")).Click();
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
