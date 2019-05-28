7ba786d259b6cd4abde2ff2a0ec8a948ed674333
e.chernov <e.chernov@01DEV78>
2019-05-28 00:50:27 -0700

    bug fixed

--- UnitTestProject1/UnitTest2.cs
+++ UnitTestProject1/UnitTest2.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support;
 using OpenQA.Selenium.Support.UI;
 
 namespace UnitTestProject1
@@ -14,67 +11,18 @@ namespace UnitTestProject1
     /// </summary>
     [TestClass]
     public class UnitTest2
-    {
-        
+    {        
         IWebDriver driver;
         WebDriverWait wait;
-        public UnitTest2()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-            driver = new ChromeDriver();
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         [Obsolete]
         public void TestMethod1()
-        {
-            //
-            // TODO: Add test logic here
-            //
+        {            
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             driver.Url = "http://localhost/litecart/admin";
+            //driver.Navigate().GoToUrl("http://localhost/litecart/admin");
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
             driver.FindElement(By.Name("login")).Click();
