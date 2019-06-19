67bdf7c0a8cfe3b6b1d91d13a95b5b1826538064
e.chernov <e.chernov@01DEV78>
2019-06-19 03:12:10 -0700

    +lecture 10

---
+++ UnitTestProject1/Lecture10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumLectures
+{
+    [TestClass]
+    public class Lecture10
+    {
+        IWebDriver driver;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Proxy proxy = new Proxy();
+            proxy.HttpProxy = "localhost:8888";
+            proxy.Kind = ProxyKind.Manual;
+           
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.Proxy = proxy;
+
+            driver = new ChromeDriver(chromeOptions);
+
+            driver.Navigate().GoToUrl("https:\\google.com");
+        }
+    }
+}
